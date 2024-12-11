232ea8a31ab89efe3450b3d872b2b87613658249
unkabas <undercannabis10@gmail.com>
2024-12-10 21:20:04 -0800

    ok

--- src/routes/routes.data.js
+++ src/routes/routes.data.js
@@ -1,15 +1,8 @@
-import layout from '../components/layout/layout'
-import counter from '../components/screens/counter/counter'
-import eventloop from '../components/screens/eventloop/eventloop'
-import pass from '../components/screens/pass/pass'
-import rest from '../components/screens/rest_api/rest'
-import todo from '../components/screens/todo/todo'
-
 export const routes = [
-	{ path: '/', component: layout },
-	{ path: '/counter', component: counter },
-	{ path: '/eventloop', component: eventloop },
-	{ path: '/pass', component: pass },
-	{ path: '/rest', component: rest },
-	{ path: '/todo', component: todo },
+	{ path: '/', component: 'Layout' },
+	{ path: '/counter', component: 'Counter' },
+	{ path: '/eventloop', component: 'EventLoop' },
+	{ path: '/pass', component: 'Pass' },
+	{ path: '/rest', component: 'Rest' },
+	{ path: '/todo', component: 'Todo' },
 ]
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,18 +1,28 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from '../components/layout/layout'
+import Counter from '../components/screens/counter/counter'
+import EventLoop from '../components/screens/eventloop/eventloop'
+import Pass from '../components/screens/pass/pass'
+import Rest from '../components/screens/rest_api/rest'
+import Todo from '../components/screens/todo/todo'
 import { routes } from './routes.data'
+
+const componentMap = {
+	Layout,
+	Counter,
+	EventLoop,
+	Pass,
+	Rest,
+	Todo,
+}
 
 const Router = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				{routes.map(route => {
-					return (
-						<Route
-							key={route.path}
-							path={route.path}
-							element={<route.component />}
-						/>
-					)
+				{routes.map(({ path, component }) => {
+					const Component = componentMap[component]
+					return <Route key={path} path={path} element={<Component />} />
 				})}
 			</Routes>
 		</BrowserRouter>
